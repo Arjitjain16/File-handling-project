337bd536729cb8e07698a7efcad3554c0cc09400
Arjitjain16 <arjitvansh16@gmail.com>
2024-03-16 00:17:35 +0530

    second commit

--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,22 @@ const globalpath = path.join(__dirname , "../" , "public" , "uploads")
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const filename = fs.readdirSync(globalpath)
-  res.render('index', {filename : filename});
+  res.render('index', {filename : filename , fileread : ""});
 });
 
-router.get('/:filename', function(req, res, next) {
-  const filename = fs.readdirSync(globalpath)
-  res.render('index', {filename : filename});
+
+router.get('/file/:filename', function(req, res, next) {
+  const fileread = fs.readFileSync(path.join(globalpath, req.params.filename), "utf8")
+
+  const files = fs.readdirSync(globalpath)
+  res.render('index', {files : files , fileread : fileread})
 });
+
+// router.get('/:filename', function(req, res, next) {
+//   const fileread = fs.readFileSync(path.join(globalpath,req.params.filename), "utf-8")
+//   const filename = fs.readdirSync(globalpath)
+//   res.render('index', {filename : filename , fileread : fileread});
+// });
 
 router.post('/createfile', function(req,res,next){
   // const filename = req.body.filename
