0e42c3bc008b11eca860adeb2fd2f5f75b2ab641
Arjitjain16 <arjitvansh16@gmail.com>
2024-03-19 23:27:13 +0530

    second commit

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const globalpath = path.join(__dirname , "../" , "public" , "uploads")
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const files = fs.readdirSync(globalpath)
-  res.render('index', {files : files , fileread : ""});
+  res.render('index', {files : files , fileread : "" , filenaam: ""});
 });
 
 
@@ -17,11 +17,11 @@ router.get('/file/:filename', function(req, res, next) {
   const fileread = fs.readFileSync(path.join(globalpath, req.params.filename),"utf-8")
 
   const files = fs.readdirSync(globalpath)
-  res.render('index', {files : files , fileread : fileread})
+  res.render('index', {files : files,fileread : fileread, filenaam:req.params.filename})
 });
 
 
-// writing files 
+// creation of files 
 router.post('/createfile', function(req,res,next){
   // const filename = req.body.filename
   const {filename} = req.body
@@ -35,6 +35,15 @@ router.get('/delete/:filename', function(req, res, next) {
   fs.unlinkSync(path.join(globalpath, req.params.filename))
   res.redirect("/")
 });
+
+// update file 
+
+router.post('/update/:filename', (req, res)=>{
+  fs.writeFileSync(path.join(globalpath, req.params.filename), req.body.fileread);
+  res.redirect(`/file/${req.params.filename}`)
+})
+
+
 
 
 
